a1cfc4d725f90a5fb7af63020a25c0a07ffcb30e
Amir Hamza <50887466+Amirhamza894@users.noreply.github.com>
2025-11-17 12:02:34 +0500

    Prevent Cypress from failing on uncaught exceptions

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -22,6 +22,7 @@ import 'cypress-plugin-steps'
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from failing the test
     return false
+    
   })
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
